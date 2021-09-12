c97df6429e4d119d9ab019c98392916021eaca18
Samir70 <sbetmouni@talktalk.net>
2021-09-12 08:59:28 +0100

    enable cors -- tested locally

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
+const whitelist = [
+    'http://localhost:5000', // for local testing
+    'https://maths-qs.web.app/', 'https://maths-qs.firebaseapp.com/'
+]
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            console.log('origin was', origin)
+            callback(new Error("Not allowed by cors"))
+        }
+    }
+}
+
+app.use('/', cors(corsOptions), (req, res, next) => {
     res.json({q:"What is the meaning of life the universe and everything?", a:42})
 })
 
