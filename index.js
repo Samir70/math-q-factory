8e3ccc87aeca90a085b72b1c7a2bf705ef9ccd40
Samir70 <sbetmouni@talktalk.net>
2021-09-29 06:44:33 +0100

    remove express and cors

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-const whitelist = [
-    'http://localhost:5000', // for local testing
-    'https://maths-qs.web.app/', 'https://maths-qs.firebaseapp.com/'
-]
-
-const corsOptions = {
-    origin: function(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            console.log('origin was', origin)
-            callback(new Error("Not allowed by cors"))
-        }
-    }
+const defaultQ = {
+    q: "What is the answer to the ultimate questions to life, the universe and everything",
+    a: 42
 }
 
-app.use('/', cors(corsOptions), (req, res, next) => {
-    res.json({q:"What is the meaning of life the universe and everything?", a:42})
-})
+const getMathsQs = () => {
+    return defaultQ
+}
 
-app.listen(3000)
+module.exports = { getMathsQs }
