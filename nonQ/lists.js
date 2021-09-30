f1505baa8a410eadc7bb38ae9cb17c6af0aefbb3
Samir70 <sbetmouni@talktalk.net>
2021-09-30 06:43:08 +0100

    add random functions

---
+++ nonQ/lists.js
@@ -0,0 +1,14 @@
+export const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+export const smallPrimes = [2, 3, 5, 7];
+
+
+export const names = [
+    'Alex', 'Arlene', 'Bruce', 'Caroline', 'Demi', 'Evette',
+    'Felix', 'Farnaz', 'Grace', 'Helen', 'Isobel', 'James', 'Jana',
+    'Katie', 'Kal', 'Louise', 'Meera', 'Muthu', 'Natalie', 'Nandini', 'Nadiya',
+    'Parvathy',
+    'Ranjit', 'Raji',
+    'Sharon', 'Shraddha', 'Steven', 'Sam',
+    'Victoria', 'Val',
+    'Zanet'
+];
