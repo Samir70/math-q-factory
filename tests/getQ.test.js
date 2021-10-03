7be67240ea15ceff4f638d87d6a018fcf721169c
Samir70 <sbetmouni@talktalk.net>
2021-10-03 08:08:17 +0100

    enable choice of Q by topic

--- tests/getQ.test.js
+++ tests/getQ.test.js
@@ -1,3 +1,7 @@
-const { getMathsQs } = require('../lib/index')
+const { getMathsQs } = require('../lib/index');
+const { topics } = require('../lib/Topics/topicList');
 
-console.log(getMathsQs())
+console.log({
+    noParams: getMathsQs(),
+    topicIsAlgebra01: getMathsQs(topics.algebra01)
+})
