cf90890bf2630818b98a82becae682c3ed50faef
Samir70 <sbetmouni@talktalk.net>
2021-10-05 06:31:52 +0100

    add randomColours function

--- tests/randFuncs.test.js
+++ tests/randFuncs.test.js
@@ -2,6 +2,7 @@ const rand = require('../lib/nonQ/randFuncs')
 
 console.log({
     randomNames: rand.randomNames(3),
+    randomColours: rand.randomColours(4),
     randomPrime: rand.randomPrime('small'),
     randomPrimeLarge: rand.randomPrime('big'),
     randomPrimeFactorisation: rand.random2357(3),
