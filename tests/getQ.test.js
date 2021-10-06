6c43781eec033e8b61b0db0027c5f0c935e753e7
Samir70 <sbetmouni@talktalk.net>
2021-10-06 06:22:21 +0100

    update tests to have more feedback

--- tests/getQ.test.js
+++ tests/getQ.test.js
@@ -1,11 +1,28 @@
 const { getMathsQs } = require('../lib/index');
-const { chapters } = require('../lib/Topics/chapterList');
+// const { chapters } = require('../lib/Topics/chapterList');
 
-console.log({
-    noParams: getMathsQs(),
-    topicIsAlgebra01: getMathsQs("algebra01"),
-    topicIsData: getMathsQs("data")
-})
-console.log({
-    topicIsData: getMathsQs("data", "mode")
-})
+const qTypes = [
+    'match', 'multiChoice', 'sort', 'shortAnswer', 'classify', 'elimination'
+]
+
+const red = "\x1b[31m"
+const yellow = "\x1b[33m"
+const white = "\x1b[37m"
+
+const topicsToTest = [
+    ['algebra', '', ''],
+    ['data', 'mode', ''],
+    ['data', 'range', '']
+]
+
+for (let [topic, subtopic, qName] of topicsToTest) {
+    q = getMathsQs(topic, subtopic, qName);
+    qDescription = [topic, subtopic, qName].join('-')
+    if (!qTypes.includes(q.qType)) {
+        console.error(red, 'getQ ', qDescription, 'has unknown qType', q.qType)
+    }
+    if (q.qFeedback === undefined) {
+        console.warn(yellow, 'getQ', qDescription, 'has no feedback')
+    }
+    console.log(white, qDescription, q)
+}
