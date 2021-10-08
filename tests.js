de759749a3113ebba49060b7bc225d9089377221
Samir70 <sbetmouni@talktalk.net>
2021-10-08 06:25:53 +0100

    modify test.js to get just one question

--- tests.js
+++ tests.js
@@ -1,6 +1,16 @@
 const { getQTests } = require('./tests/getQ.test');
+const { getMathsQs } = require('./lib/index');
 const { chapStructureTest, sectStructreTest } = require('./tests/chapSecStructure.test')
 
-getQTests();
-chapStructureTest();
-sectStructreTest();
+const args = process.argv.slice(2)
+
+if (args.length > 0) {
+    while (args.length < 3) {args.push('')}
+    let [chapter, section, qName] = args;
+    console.log('getting a ', args.join('-'), 'question')
+    console.log(getMathsQs(chapter, section, qName))
+} else {
+    chapStructureTest();
+    sectStructreTest();
+    getQTests();
+}
