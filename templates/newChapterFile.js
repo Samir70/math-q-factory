9b0be8703d193d2bd47df394792103071ea5c249
Samir70 <sbetmouni@talktalk.net>
2021-10-11 06:14:53 +0100

    update newChapterFile

--- templates/newChapterFile.js
+++ templates/newChapterFile.js
@@ -1,11 +1,12 @@
 /**
- * This boilerplate for a new chapter.
+ * This is boilerplate for a new chapter.
  * Take a look at lib/topics/data/dataQs.js for an example of how it is used.
  * Once it is working, you can delete the comments
+ * variables in ALLCAPS need to be changed
  */
 
 // import the sections that this chapter has.
-const { } = require();
+const { SECTION_LIST } = require();
 const { findQGetter } = require('../../findQGetter');
 
 // Update below line and uncomment
@@ -13,7 +14,7 @@ const { findQGetter } = require('../../findQGetter');
 
 // give a name to this function eg: for chapter data, use dataQs
 // upddate the SECTION_LIST variable name to match what you imported above
-CHANGENAME = (section = '', qName = '') => {
+const CHANGENAME = (section = '', qName = '') => {
     let qGetter = findQGetter('sectionName', section, SECTION_LIST);
     if (qGetter === -1) { return defaultQ }
     let question = qGetter(qName);
@@ -21,6 +22,9 @@ CHANGENAME = (section = '', qName = '') => {
 }
 
 // update below and uncomment
+// remember to register the chapterName in the 
+// -- qPathList.js file (to enable tests and tell users it is available) and 
+// -- chapterList.js file (so the program can find the path)
 module.exports = {
     // chapterName: 'USE_THE_CHAPTER_NAME_THAT_WILL_BE_PART_OF_PATH_TO_FINAL_Q',
     // qGetter: CHANGENAME
