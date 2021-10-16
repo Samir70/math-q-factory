03afc5d99e681b8258c5f8362020ed3832755510
Samir70 <sbetmouni@talktalk.net>
2021-10-16 08:12:08 +0100

    tests for myMath.gcd

--- tests/myMath.tests.js
+++ tests/myMath.tests.js
@@ -2,7 +2,7 @@ const myMath = require('../lib/nonQ/myMathFuncs');
 const { red, yellow, green, white } = require('./colours');
 
 const roundDPTests = [
-    [4.566, 4.57, 2], [4.565, 4.57, 2], [4.562, 4.56, 2], [3.996, 4, 2], 
+    [4.566, 4.57, 2], [4.565, 4.57, 2], [4.562, 4.56, 2], [3.996, 4, 2],
     [1.005, 1.01, 2], [59.385, 59.39, 2],
     // one decimal place
     [6.67, 6.7, 1], [6.65, 6.7, 1], [6.64, 6.6, 1], [7.95, 8, 1],
@@ -13,7 +13,7 @@ const testRoundDP = () => {
     let allPass = true;
     for (let [a, b, dp] of roundDPTests) {
         if (myMath.roundDP(a, dp) !== b) {
-            console.error(red, `myMath.twoDecimal failed for ${a} exprected ${b} got ${myMath.roundDP(a, dp)}`)
+            console.error(red, `myMath.roundDP failed for ${a} exprected ${b} got ${myMath.roundDP(a, dp)}`)
             allPass = false
         }
     }
@@ -21,6 +21,27 @@ const testRoundDP = () => {
     console.log(white)
 }
 
-exports.myMathTests = () => {
-    testRoundDP()
+const gcdTests = [
+    // [[a, b], gcd(a, b)]
+    [[1, 13], 1], [[4, 8], 4], [[7, 15], 1], [[0, 6], 6], 
+    [[1470, 588], 294], [[210, 294], 42], [[882, 378], 126],
+    [[441, 189, 315], 63], [[392, 1372, 980], 196]
+]
+
+const testGCD = () => {
+    console.log('Testing myMath.gcd()');
+    let allPass = true;
+    for (let [nums, ans] of gcdTests) {
+        if (myMath.gcd(nums) !== ans) {
+            console.error(red, `myMath.gcd failed for ${nums}. Expected ${ans} got ${myMath.gcd(nums)}`)
+            allPass = false;
+        }
+    }
+    if (allPass) {console.log(green, 'myMath.gcd passed all tests')}
+    console.log(white)
 }
+
+exports.myMathTests = () => {
+    testGCD()
+    testRoundDP(); 
+};
