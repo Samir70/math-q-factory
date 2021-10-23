fd075307af2050eaad8c32c32b3d230afa481b4a
Samir70 <sbetmouni@talktalk.net>
2021-10-23 08:11:59 +0100

    add tests for formatting cFracs

--- tests/formatfuncs.test.js
+++ tests/formatfuncs.test.js
@@ -1,4 +1,4 @@
-const { joinList } = require('../lib/nonQ/formatFuncs');
+const { joinList, cFracFormat } = require('../lib/nonQ/formatFuncs');
 const { red, green, white } = require('./colours');
 
 const joinListTests = [
@@ -24,6 +24,29 @@ const testGiveNames = () => {
     console.log(white)
 }
 
+const cFracFormatTests = [
+    { arr: [4, 5, 6, 7], cFrac: '[4; 5, 6, 7]' },
+    { arr: [4], cFrac: '[4]' },
+    { arr: [8, 1], cFrac: '[8; 1]' }
+]
+
+const testCFracFormat = () => {
+    console.log('Testing formatFuncs.cFracFormat')
+    let allPass = true;
+    for (let test of cFracFormatTests) {
+        if (cFracFormat(test.arr) !== test.cFrac) {
+            console.error(red, `cFracFormat failed for ${test.arr}`)
+            console.error(red, `expected: ${test.cFrac}, got ${cFracFormat(test.arr)}`)
+            allPass = false
+        }
+    }
+    if (allPass) {
+        console.log(green, 'formatFuncs.cFracFormat passed all tests')
+    }
+    console.log(white)
+}
+
 exports.formatFuncsTests = () => {
-    testGiveNames()
+    testGiveNames();
+    testCFracFormat();
 }
