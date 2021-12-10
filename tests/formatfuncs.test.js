f107f270160a17327311f1032b28b409f4ed6ddb
Samir70 <sbetmouni@talktalk.net>
2021-12-10 06:17:49 +0000

    add tests for makeExpression

--- tests/formatfuncs.test.js
+++ tests/formatfuncs.test.js
@@ -1,4 +1,4 @@
-const { joinList, cFracFormat } = require('../lib/nonQ/formatFuncs');
+const { joinList, cFracFormat, makeExpression } = require('../lib/nonQ/formatFuncs');
 const { red, green, white } = require('./colours');
 
 const joinListTests = [
@@ -46,7 +46,37 @@ const testCFracFormat = () => {
     console.log(white)
 }
 
+const makeExpressionTests = [
+    { coefs: [6, 7, 3, -9, 4], vars: ['a', 'c', 'b', 'a', 'c'], expectedAns: '6a+7c+3b-9a+4c' },
+    { coefs: [6, 7, 3, -9, 4], vars: ['a', 'c', '', 'a', 'c'], expectedAns: '6a+7c+3-9a+4c' },
+    { coefs: [1, 3, -2, -4], vars: ['a', 'b', 'a', 'c'], expectedAns: 'a+3b-2a-4c' },
+    { coefs: [7, 3, -2, -1], vars: ['a', 'b', 'a', 'c'], expectedAns: '7a+3b-2a-c' },
+    { coefs: [1, 4, 0, 2], vars: ['a', 'b', 'a', 'b'], expectedAns: 'a+4b+2b' },
+    { coefs: [0, 4, 4, 2], vars: ['a', 'b', 'a', 'b'], expectedAns: '4b+4a+2b' },
+    { coefs: [0, 0, 0, 0], vars: ['a', 'b', 'a', 'b'], expectedAns: '0' },
+    { coefs: [7, 6, 4], vars: [], expectedAns: null },
+    { coefs: [], vars: ['a', 'b', 'a'], expectedAns: null },
+    { coefs: [5, 2, -5, 1, 5], vars: ['a', 'c', 'b', 'c'], expectedAns: '5a+2c-5b+c' },
+    { coefs: [5, 2, -5, 1], vars: ['a', 'c', 'b', 'c', 'a'], expectedAns: '5a+2c-5b+c' }
+];
+const testMakeExpressions = () => {
+    console.log('Testing formatFuncs.makeExpressions')
+    let allPass = true;
+    for (let test of makeExpressionTests) {
+        if (makeExpression(test.coefs, test.vars) !== test.expectedAns) {
+            console.error(red, `makeExpression failed for ${JSON.stringify(test)}`)
+            console.error(red, `expected: ${test.expectedAns}, got ${makeExpression(test.coefs, test.vars)}`)
+            allPass = false
+        }
+    }
+    if (allPass) {
+        console.log(green, 'formatFuncs.cFracFormat passed all tests')
+    }
+    console.log(white)
+}
+
 exports.formatFuncsTests = () => {
     testGiveNames();
     testCFracFormat();
+    testMakeExpressions();
 }
