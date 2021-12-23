51c1fcc9a362da680d398b8f8721f23e8f3f21a3
Samir70 <sbetmouni@talktalk.net>
2021-12-23 11:16:18 +0000

    fix path specific tests for getQ

--- tests.js
+++ tests.js
@@ -10,15 +10,16 @@ const args = process.argv.slice(2)
 if (args.length > 0 && args[0] !== '--verbose') {
     while (args.length < 3) { args.push('') }
     let [chapter, section, qName] = args;
-    let topics = topicsToTest.filter(t => t[0] === chapter);
-    if (section !== '') { topics = topics.filter(t => t[1] === section) }
-    if (qName !== '') { topics = topics.filter(t => t[2] === qName) }
+    let topics = topicsToTest.filter(t => t.path[0] === chapter);
+    if (section !== '') { topics = topics.filter(t => t.path[1] === section) }
+    if (qName !== '') { topics = topics.filter(t => t.path[2] === qName) }
     if (topics.length === 0) {
         console.log('No question found at path', args.join('-'))
         let [c, s, q] = args;
         console.log(getMathsQs(c, s, q))
     }
-    for (let [c, s, q] of topics) {
+    for (let topic of topics) {
+        let [c, s, q] = topic.path
         console.log('getting a ', [c, s, q].join('-'), 'question');
         console.log(getMathsQs(c, s, q))
     }
