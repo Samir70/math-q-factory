d59be9fa0fe0b7258e4760b78a76491dd4111d5d
Samir70 <sbetmouni@talktalk.net>
2022-06-25 16:18:08 +0100

    make a test template for use with testRunner

---
+++ templates/testsObj.js
@@ -0,0 +1,12 @@
+// change everything in CAPS
+const TESTNAME = {
+    name: "NAME_FOR_THIS_TEST",
+    compareFunc: "PICK_FUNC_FROM_THOSE_LISTED_IN_TESTRUNNER",
+    func: FUNC_BEING_TESTED,
+    tests: [
+      // DESCRIBE WHAT THE ARGS DO, SHOULD BE SIM TO FUNC_BEING_TESTED
+      // each object below is a test
+      { args: [], expect: [] },
+      { args: [], expect: [] }
+    ]
+  }
