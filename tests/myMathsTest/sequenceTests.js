a0ad613c26469b67bea46e97c1e58c456f89f0df
Samir70 <46189442+Samir70@users.noreply.github.com>
2022-06-27 10:41:25 +0100

    add tests for cubic sequences

--- tests/myMathsTest/sequenceTests.js
+++ tests/myMathsTest/sequenceTests.js
@@ -1,4 +1,5 @@
 const {makeSeq} = require('../../lib/nonQ/sequenceFunctions');
+// need tests for ['', '', '', '', '', 'two-step', 'harmonic']
 
 const linearSeqTests = {
   name: "Testing linear sequences",
@@ -46,6 +47,20 @@ const quadraticSeqTests = {
     {args: [5, 1, 0, 0], expect: [1, 4, 9, 16, 25]  },
     {args: [5, 1, 1, 0], expect: [2, 6, 12, 20, 30]  },
     {args: [5, 1, 2, 1], expect: [4, 9, 16, 25, 36] }
+  ]
+}
+
+const cubicSeqTests = {
+  name: "Testing cubic sequences",
+  compareFunc: "compareArrays",
+  func: makeSeq.linear,
+  tests: [
+    // args are n, a, b, c, d = how many, coefs of x^3, x^2, x, const
+    {args: [6, 1, 0, 0, 0], expect: [1, 8, 27, 64, 125, 216] },
+    {args: [6, 1, 0, 0, 3], expect: [4, 11, 30, 67, 128, 219]  },
+    {args: [6, 1, 0, 1, 3], expect: [5, 13, 33, 71, 133, 225]  },
+    {args: [6, 1, 1, 1, 3], expect: [6, 17, 42, 87, 158, 261]  }, 
+    {args: [6, 2, 3, 4, 5], expect: [14, 41, 98, 197, 350, 569]  }
   ]
 }
 
