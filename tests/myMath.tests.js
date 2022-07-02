6361f703a36c51c155ad380d4d1dc4fd7e202861
Samir70 <sbetmouni@talktalk.net>
2022-07-02 21:47:17 +0100

    refactor rounding tests to use testRunner

--- tests/myMath.tests.js
+++ tests/myMath.tests.js
@@ -1,28 +1,9 @@
 const myMath = require('../lib/nonQ/myMathFuncs');
 const rFuncs = require('../lib/nonQ/randFuncs');
 const { seqTests } = require('./myMathsTest/sequenceTests');
+const { roundingTests } = require('./myMathsTest/roundingTests');
 const { testRunner, displayResult } = require('./testRunner');
 const { red, yellow, green, white } = require('./colours');
-
-const roundDPTests = [
-    [4.566, 4.57, 2], [4.565, 4.57, 2], [4.562, 4.56, 2], [3.996, 4, 2],
-    [1.005, 1.01, 2], [59.385, 59.39, 2],
-    // one decimal place
-    [6.67, 6.7, 1], [6.65, 6.7, 1], [6.64, 6.6, 1], [7.95, 8, 1],
-    [1.05, 1.1, 1]
-]
-const testRoundDP = () => {
-    console.log('Testing myMath.roundDP(x, dp)')
-    let allPass = true;
-    for (let [a, b, dp] of roundDPTests) {
-        if (myMath.roundDP(a, dp) !== b) {
-            console.error(red, `myMath.roundDP failed for ${a} exprected ${b} got ${myMath.roundDP(a, dp)}`)
-            allPass = false
-        }
-    }
-    if (allPass) { console.log(green, 'myMath.roundDP passed all tests') }
-    console.log(white)
-}
 
 const penceToPoundsTests = [
     [324, '£3.24'], ['240', '£2.40'], [600, '£6'], [32, '£0.32']
@@ -210,5 +191,8 @@ exports.myMathTests = (showAll) => {
     testConvergents();
     rndTests4cFracs();
     testMultInv();
-    testRunner(seqTests).forEach(r => displayResult(r, showAll))
+    const tests = [seqTests, roundingTests];
+    tests.forEach(t => {
+        testRunner(t).forEach(r => displayResult(r, showAll))
+    })
 };
--- tests/myMathsTest/roundingTests.js
+++ tests/myMathsTest/roundingTests.js
@@ -1,4 +1,4 @@
-const { roundToWithin, roundDP  } = require('../../lib/nonQ/myMathFuncs');
+const { roundToWithin, roundDP } = require('../../lib/nonQ/myMathFuncs');
 
 const rountToWithinTests = {
   name: "Rounding dodgy javaScript division to useful number of decimals",
@@ -6,9 +6,9 @@ const rountToWithinTests = {
   func: roundToWithin,
   tests: [
     // args are [javaScript division] expect: [good decimal answer]
-    {args: [9/5, 0.00001], expect: 1.8},
-    {args: [9/4, 0.0001], expect: 2.25},
-    {args: [9/4, 0.1], expect: 2.3}
+    { args: [9 / 5, 0.00001], expect: 1.8 },
+    { args: [9 / 4, 0.0001], expect: 2.25 },
+    { args: [9 / 4, 0.1], expect: 2.3 }
   ]
 }
 
@@ -18,19 +18,19 @@ const roundToDecPlaces = {
   func: roundDP,
   tests: [
     // args are (value, decimal places)
-    [, 6.7, 1], [, 6.7, 1], [, 6.6, 1], [, 8, 1],
-    [, 1.1, 1]
-    {args: [4.566, 2], expect: 4.57 },
-    {args: [4.565, 2], expect: 4.57 },
-    {args: [4.562, 2], expect: 4.56 },
-    {args: [3.996], expect: 4 },
-    {args: [1.005, 2], expect: 1.01 },
-    {args: [59.385, 2], expect: 59.39 },
-    {args: [6.67, 1], expect: 6.7 },
-    {args: [6.65, 1], expect: 6.7 },
-    {args: [6.64, 1], expect: 6.6 },
-    {args: [7.95, 1], expect: 8 },
-    {args: [1.05, 1], expect: 1.1 }
+    { args: [4.566, 4], expect: 4.566 },
+    { args: [4.566, 3], expect: 4.566 },
+    { args: [4.566, 2], expect: 4.57 },
+    { args: [4.565, 2], expect: 4.57 },
+    { args: [4.562, 2], expect: 4.56 },
+    { args: [3.996, 2], expect: 4 },
+    { args: [1.005, 2], expect: 1.01 },
+    { args: [59.385, 2], expect: 59.39 },
+    { args: [6.67, 1], expect: 6.7 },
+    { args: [6.65, 1], expect: 6.7 },
+    { args: [6.64, 1], expect: 6.6 },
+    { args: [7.95, 1], expect: 8 },
+    { args: [1.05, 1], expect: 1.1 }
   ]
 }
 
